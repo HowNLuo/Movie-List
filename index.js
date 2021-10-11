b3076534098164087e8321bf0e8988febd9b5836
HowNLuo <xji6cl4p@gmail.com>
2021-10-12 00:26:58 +0800

    add: Search function

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const BASE_URL = 'https://movie-list.alphacamp.io'
-const INDEX_URL = BASE_URL + '/api/v1/movies'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
 const POSTER_URL = BASE_URL + '/posters/'
 
 const movies = []
 
 const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
 
 function renderMovieList(data) {
   let rawHTML = ''
@@ -20,7 +22,7 @@ function renderMovieList(data) {
             </div>
             <div class="card-footer">
               <button class="btn btn-primary btn-show-movie" data-toggle="modal"
-                data-target="#movie-modal">More</button>
+                data-target="#movie-modal" data-id="${item.id}">More</button>
               <button class="btn btn-info btn-add-favorite">+</button>
             </div>
           </div>
@@ -31,6 +33,44 @@ function renderMovieList(data) {
 
   dataPanel.innerHTML = rawHTML
 }
+
+function showMovieModal(id) {
+  const movieTitle = document.querySelector('#movie-modal-title')
+  const movieImage = document.querySelector('#movie-modal-image')
+  const moviedate = document.querySelector('#movie-modal-date')
+  const movieDescription = document.querySelector('#movie-modal-description')
+
+  axios.get(INDEX_URL + id).then((response) => {
+    const data = response.data.results
+    movieTitle.innerText = data.title
+    movieImage.innerHTML = `<img src="${POSTER_URL + data.image}" alt="movie-poster" class="img-fluid">`
+    moviedate.innerText = 'Release date: ' + data.release_date
+    movieDescription.innerText = data.description
+
+  })
+}
+
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModal(Number(event.target.dataset.id))
+  }
+})
+
+searchForm.addEventListener('submit', function onSearchFromSubmitted(event) {
+  event.preventDefault()
+  const keyword = searchInput.value.trim().toLowerCase()
+  let filteredMovies = []
+
+  filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+
+  if (!filteredMovies.length) {
+    return alert(`Cannot find movie with keyword: ${keyword}`)
+  }
+
+  renderMovieList(filteredMovies)
+})
 
 axios.get(INDEX_URL).then((response) => {
   movies.push(...response.data.results)
